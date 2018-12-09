d660c2cacf999ff547c4148f72bf506677e1a9d5
coco <abalon.kn@gmail.com>
2018-12-09 17:06:06 +0000

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  configureWebpack: {
+    // other webpack options to merge in ...
+  },
+  // devServer Options don't belong into `configureWebpack`
+  devServer: {
+    host: '0.0.0.0',
+    port: '8081',
+    disableHostCheck: true
+  },
+};
